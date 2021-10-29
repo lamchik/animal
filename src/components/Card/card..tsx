d1a4ae050ec725d8af2480e4f253dbe6d3c49b10
lamchik <lamaralanchava@gmail.com>
2021-10-29 18:35:28 +0300

    added icons

--- src/components/Card/card..tsx
+++ src/components/Card/card..tsx
@@ -1,5 +1,12 @@
-import {loadCard} from "../../api/apiCard";
 import {ApiCard} from "../../domain/card";
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import IconButton from '@mui/material/IconButton';
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 type Props = {
   card: ApiCard;
@@ -7,9 +14,29 @@ type Props = {
 
 export function Card({ card }: Props) {
   return (
-    <div>
-      <p>{card.author}</p>
-      <img src={card.download_url} alt='image'/>
-    </div>
+    <ImageListItem key={card.id}>
+      <img
+        src={`${card.download_url}?w=248&fit=crop&auto=format`}
+        // srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+        alt='image'
+        loading="lazy"
+      />
+      <ImageListItemBar
+        title={card.author}
+        subtitle={card.url}
+        actionIcon={
+          <IconButton
+            sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+            aria-label={`info about`}
+          >
+            <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+          </IconButton>
+        }
+      />
+    </ImageListItem>
+    // <div>
+    //   <p>{card.author}</p>
+    //   <img src={card.download_url} alt='image'/>
+    // </div>
   )
 }
--- src/components/CardList/cardList.tsx
+++ src/components/CardList/cardList.tsx
@@ -3,6 +3,9 @@ import {useCallback, useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {loadCard} from "../../api/apiCard";
 import {State} from "../../store/images";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ListSubheader from '@mui/material/ListSubheader';
 
 export function CardList() {
   const dispatch = useDispatch()
@@ -10,8 +13,8 @@ export function CardList() {
   const loadCards = useCallback(() => {
     loadCard()
       .then((res) => {
-      dispatch({type: 'loaded', value: res})
-    })
+        dispatch({type: 'loaded', value: res})
+      })
       .catch((err) => {
         dispatch({type: 'error', value: err.toString()})
       })
@@ -20,12 +23,33 @@ export function CardList() {
   useEffect(() => {
     loadCards();
   })
-
   return (
-    <div>
-      {allCards.map((card) => {
-        return <Card card={card}/>
-      })}
-    </div>
+    <ImageList >
+      <ImageListItem key="Subheader" cols={5}>
+        <ListSubheader component="div">Images for life</ListSubheader>
+      </ImageListItem>
+      {allCards.map((item) => (
+        <Card key={item.id} card={item}/>
+      ))}
+    </ImageList>
   )
 }
+
+//   return (
+//     <ImageList sx={{ width: 500, height: 450 }}>
+//       <ImageListItem key="Subheader" cols={2}>
+//         <ListSubheader component="div">December</ListSubheader>
+//       </ImageListItem>
+//       {allCards.map((item) => {
+//         return <Card key={item.id} card={item}/>
+//       }
+//         </ImageList>
+//       )}
+//
+//     {/*<div>*/}
+//     {/*  {allCards.map((card) => {*/}
+//     {/*    return <Card card={card}/>*/}
+//     {/*  })}*/}
+//     {/*</div>*/}
+//   )
+// }
